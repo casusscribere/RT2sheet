20ade8bbfeef913da47c4d72d37c58d1ae42478b
Argagarg <kirk.lundblade@gmail.com>
2017-06-16 22:28:07 -0500

    ranged 1.1
    Added hit locations, jamming, righteous fury, etc.

--- ranged40k.js
+++ ranged40k.js
@@ -11,31 +11,54 @@ var ranged40kNamespace = ranged40kNamespace || {};
 
 ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shotsel, single, semi, full, numdice, dice, dmg, pen) {
     var roll = randomInteger(100);
+    //var roll = 94;
     var modTarget = 0;
     var degOfSuc =0;
     var numhits = 100;
     var error=false;
-    var errortext="ERROR: INVALID FIRING MODE";
+    var errortext="ERROR: GENERIC";
     var hit=true;
+    var jam=false;
     var output='';
     var semicalc=0;
     var boundmod=0;
+    var i=0;
+    var dmgstring='';
+    var temp=0;
+    var temp2=0;
+    var substring='';
     
 
-    //Add firing mode modifier
+    //Add firing mode modifier & check for Jams
     if(shotsel == 0){
         //If the user selected Standard Attack
-        boundmod = 10;
+        if(roll>=96){
+            jam=true;
+        }
+        else{
+            boundmod = 10;
+        }
         
     } else if (shotsel == 1){
         //If the user selected Semi-Auto
-        boundmod = 0;
+        if(roll>=94){
+            jam=true;
+        }
+        else{
+            boundmod = 0;
+        }
     } else if (shotsel == 2){
         //If the user selected Full-Auto
-        boundmod = -10;
+        if(roll>=94){
+            jam=true;
+        }
+        else{
+            boundmod = -10;
+        }
     }
     else{
         error=true;
+        errortext="ERROR: Invalid Shot Selection primary"
     }
     
     
@@ -58,6 +81,9 @@ ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shot
     } else if (range==5){
         //Extreme Range
         boundmod = parseInt(boundmod)+parseInt(-30);
+    } else{
+        error=true;
+        errortext="ERROR: Invalid Range Selection"
     }
     
     
@@ -86,16 +112,24 @@ ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shot
     
     
     //Calculate Degrees of Success
-    if(roll <= modTarget) {
+    if(roll <= modTarget && jam==false) {
         degOfSuc = (Math.floor(modTarget/10) - Math.floor(roll/10)) + 1;
-        output2 = '<span style="color:green">' + token + ' succeeds by <B>' + degOfSuc + ' degree(s)</B>.</span> ';
+        output2 = '<span style="color:green">' + token + ' hits with <B>' + degOfSuc + ' degree(s)</B>.</span> ';
         hit=true;
     }
-    else {
+    else if(roll > modTarget && jam==false){
         degOfSuc = (Math.floor(roll/10) - Math.floor(modTarget/10)) + 1;
-        output2 = '<span style="color:red">' + token + ' fails by <B>' + degOfSuc + ' degree(s)</B></span>. ';
+        output2 = '<span style="color:red">' + token + ' misses by <B>' + degOfSuc + ' degree(s)</B></span>. ';
         numhits=0;
         hit=false;
+    } else if (jam==true){
+        output2 = '<span style="color:red">' + token + ' jams his weapon! </span>. ';
+        numhits=0;
+        hit=false;
+        
+    } else{
+        error=true;
+        errortext="ERROR: Failed DoS Calculation"
     }
     
     
@@ -108,6 +142,7 @@ ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shot
             numhits = 1;     
         } else{
             error=true;
+            errortext="ERROR: INVALID FIRING MODE";
             numhits=0;
         }
     } else if (shotsel == 1 && hit==true && error==false){
@@ -115,6 +150,7 @@ ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shot
         if(parseInt(semi)==0){
             numhits = 0;
             error=true;
+            errortext="ERROR: INVALID FIRING MODE";
         }
         else if(semicalc < semi)
         {
@@ -128,6 +164,7 @@ ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shot
         if(parseInt(full)==0){
             numhits = 0;
             error=true;
+            errortext="ERROR: INVALID FIRING MODE";
         }
         else if(degOfSuc < full)
         {
@@ -143,14 +180,48 @@ ranged40kNamespace.rollResult = function(token, attribute, modifier, range, shot
     
     
     
+    //Determine Initial Hit Location
+    temp = Math.floor(roll/10); //Store 10s place
+    temp2 = roll - temp*10;     //Store 1s place
+    temp = temp2*10+temp;       //swap 10s and 1s place
+    //temp = 90;
+    if(temp <= 10) {
+        var hitloc = ["Head","Head","Left Arm","Body","Right Arm","Body","Body","Body","Body","Body"];
+    } else if(10 < temp && temp <= 20){
+        var hitloc = ["Right Arm","Right Arm","Body","Head","Body","Left Arm","Left Arm","Left Arm","Left Arm","Left Arm"];
+    } else if(20 < temp && temp <= 30){
+        var hitloc = ["Left Arm","Left Arm","Body","Head","Body","Right Arm","Right Arm","Right Arm","Right Arm","Right Arm"];
+    } else if(30 < temp && temp <= 70){
+        var hitloc = ["Body","Body","Right Arm","Head","Left Arm","Body","Body","Body","Body","Body"];
+    } else if(70 < temp && temp <= 85){
+        var hitloc = ["Right Leg","Right Leg","Body","Right Arm","Head","Body","Body","Body","Body","Body"];
+    } else if(85 < temp && temp <= 100){
+        var hitloc = ["Left Leg","Left Leg","Body","Left Arm","Head","Body","Body","Body","Body","Body"];
+    } else{
+        error=true;
+        errortext="ERROR: BAD HIT LOCATION";
+    }
+    
+    
+    
+    //Compute Damage substring
+    for(i=1; i <=numhits; i++){
+    dmgstring = dmgstring + " --Damage: *"+i+"|[[ [$Dmg"+i+"] "+parseInt(numdice)+"d"+parseInt(dice)+"+"+parseInt(dmg)+"]] Pen "+parseInt(pen)+" to the "+hitloc[i];
+    //dmgstring = dmgstring + " --Damage: *"+i+"|[[ [$Dmg"+i+"] 3d10d1 +5 ]] Pen "+parseInt(pen)+" to the "+hitloc[i];
+    dmgstring = dmgstring + " --?? $Dmg"+i+".tens > 0 ?? Righteous Fury: *"+i+"| [[1d5]] critical damage to the "+hitloc[i];
+    }
+    
+    
+    
     //Return output
     if(error==true){
       output = errortext; 
     }
     else {
-        output ="!power {{--format|skill --titlefontshadow|none --name|"+token+" --leftsub| Check "+modTarget+" --rightsub| Roll "+roll+" --Effect:|"+output2+" --Damage:#"+numhits+"|[["+parseInt(numdice)+"d"+parseInt(dice)+"+"+parseInt(dmg)+"]] }}"
+        //output ="!power {{--format|skill --titlefontshadow|none --name|"+token+" --leftsub| Check "+modTarget+" --rightsub| Roll "+roll+" --Effect:|"+output2+" --Damage:#"+numhits+"|[[ [$Dmg] "+parseInt(numdice)+"d"+parseInt(dice)+"+"+parseInt(dmg)+"]] --?? $Dmg.base == "+parseInt(dice)+" ?? Critical Hit:| Add [[1d8]] slashing damage }}";
+        //output ="!power {{--format|skill --titlefontshadow|none --name|"+token+" --leftsub|Check "+modTarget+" --rightsub| Roll "+roll+" --Roll:| [! "+roll+" !] vs [! "+modTarget+" !] --Result:|"+output2+dmgstring+"}}"
+        output ="!power {{--format|ranged --titlefontshadow|none --name|"+token+" --leftsub| Ballistic Skill Check  --rightsub| My Weapon --Roll:| [! "+roll+" !] vs [! "+modTarget+" !] --Result:|"+output2+dmgstring+"}}"
     }
-    //var output ="!power {{--format|skill --titlefontshadow|none --name|"+token+" --leftsub|Check "+modTarget+" --rightsub| Roll "+roll+" --Effect|"+output2+" }}"
     return output;
 }
 
